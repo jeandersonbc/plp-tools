a9146672cdec74ccb6343756269643ffeb34600b
Jeanderson Candido <jeandersonbc@gmail.com>
2016-05-31 09:55:25 -0300

    Adicionado editor na UI

--- ui/src/app/require.config.js
+++ ui/src/app/require.config.js
@@ -9,7 +9,7 @@ var require = {
         "knockout":             "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
         "signals":              "bower_modules/js-signals/dist/signals.min",
-        "text":                 "bower_modules/requirejs-text/text"
+        "text":                 "bower_modules/requirejs-text/text",
     },
     shim: {
         "bootstrap": { deps: ["jquery"] }
