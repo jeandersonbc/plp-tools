93c15e73453f7d569e92b4a46583fcd457c676e3
Jeanderson Candido <jeandersonbc@gmail.com>
2016-05-31 12:51:20 -0300

    Atualizado campos da pagina principal

--- ui/src/components/home-page/home.js
+++ ui/src/components/home-page/home.js
@@ -4,15 +4,18 @@ import homeTemplate from 'text!./home.html';
 class HomeViewModel {
     constructor(route) {
         this.selectedLanguage = ko.observable();
+        this.stdinContent = ko.observable();
     }
     updateEditor(data, event) {
-        // TODO: update to dynamically loading syntax highlight
+        // TODO: update to load dynamically the syntax highlight
     }
     clear() {
         console.log("Clearing fields");
     }
     execute() {
+        console.log("execute()")
         console.log(this.selectedLanguage());
+        console.log(this.stdinContent());
     }
 }
 
