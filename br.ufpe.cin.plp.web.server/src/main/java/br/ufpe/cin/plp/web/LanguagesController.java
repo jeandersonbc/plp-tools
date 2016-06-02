1afc5cbabc4234de62dc443cbc5590717d7a2b00
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-02 07:37:03 -0300

    Atualizado controlador e service

--- br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/LanguagesController.java
+++ br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/LanguagesController.java
@@ -1,9 +1,13 @@
 package br.ufpe.cin.plp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufpe.cin.plp.web.model.CompilerOutput;
@@ -16,10 +20,10 @@ public class LanguagesController {
 	@Autowired
 	LanguagesService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public CompilerOutput compile(@RequestParam String language,
-			@RequestParam(required = false, defaultValue = "") String input) {
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<CompilerOutput> compile(@RequestParam String language,
+			@RequestParam String source, @RequestParam(required = false, defaultValue = "") String input) {
 
-		return service.compile(language, input);
+		return new ResponseEntity<>(service.compile(language, source, input), HttpStatus.OK);
 	}
 }
--- br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/services/LanguagesService.java
+++ br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/services/LanguagesService.java
@@ -10,12 +10,10 @@ import br.ufpe.cin.plp.web.model.CompilerOutput;
 @Component
 public class LanguagesService {
 
-	public CompilerOutput compile(String language, String input) {
+	public CompilerOutput compile(String language, String source, String input) {
 		CompilerOutput compilerOutput = new CompilerOutput();
 		compilerOutput.setSourceLanguage(language);
 
-		// FIXME: mocked source
-		String source = "1 + 2";
 		Interpreter interpreter = Interpreters.getByLanguageCode(language);
 		String result = null;
 		try {
