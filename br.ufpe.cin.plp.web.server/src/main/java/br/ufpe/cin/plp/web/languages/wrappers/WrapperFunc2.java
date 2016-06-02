c5ae0a6b42398c59bc9e2bf625a06aa587cc1c42
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-02 03:52:53 -0300

    Adicionado TODO em wrapper de Funcional 2

--- br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc2.java
+++ br.ufpe.cin.plp.web.server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc2.java
@@ -6,6 +6,7 @@ public class WrapperFunc2 implements Interpreter {
 
 	@Override
 	public String run(String source, String input) {
+		// TODO: Implementar quando tiver Parser disponivel
 		return "Parser nao encontrado";
 	}
 
