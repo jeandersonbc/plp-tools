8080953d52cf7148c3dadf2434189d85a07c853b
leusonmario <leusonmario@gmail.com>
2016-06-06 20:51:52 -0300

    Adição de ações nos Wrappers das linguagens funcionais (Funcional 1, 2 e 3)

--- server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc1.java
+++ server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc1.java
@@ -1,14 +1,34 @@
 package br.ufpe.cin.plp.web.languages.wrappers;
 
+import java.io.ByteArrayInputStream;
+
+import lf1.plp.functional1.Programa;
+import lf1.plp.functional1.parser.Func1Parser;
+import lf1.plp.functional1.parser.ParseException;
 import br.ufpe.cin.plp.web.languages.GenericParseException;
 import br.ufpe.cin.plp.web.languages.Interpreter;
 
 public class WrapperFunc1 implements Interpreter {
+	private Func1Parser func1Parser;
 
 	@Override
 	public String run(String source, String input) throws GenericParseException {
-		// TODO: Implementar quando tiver Parser disponivel
-		return "Parser nao encontrado";
+		ByteArrayInputStream fis = new ByteArrayInputStream(source.getBytes());
+		Programa prog;
+		if (func1Parser == null) {
+			func1Parser = new Func1Parser(fis);
+		} else {
+			Func1Parser.ReInit(fis);
+		}
+		try {
+			prog = Func1Parser.Input();
+		} catch (ParseException e) {
+			throw new GenericParseException();
+		}
+
+		return (prog.checaTipo() ? prog.executar().toString()
+				: "Erro det tipos!");
+
 	}
 
 }
