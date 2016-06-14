2fd0438d6f9560eca7c086556f631beb6cc45b9d
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-14 15:14:28 -0300

    Revert "Adição de ações nos Wrappers das linguagens funcionais (Funcional 1, 2 e 3)"
    This reverts commit 8080953d52cf7148c3dadf2434189d85a07c853b.

--- server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc2.java
+++ server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc2.java
@@ -1,32 +1,13 @@
 package br.ufpe.cin.plp.web.languages.wrappers;
 
-import java.io.ByteArrayInputStream;
-
-import lf2.plp.functional2.parser.Func2Parser;
-import br.ufpe.cin.plp.web.languages.GenericParseException;
 import br.ufpe.cin.plp.web.languages.Interpreter;
 
 public class WrapperFunc2 implements Interpreter {
-	private Func2Parser func2Parser;
 
 	@Override
-	public String run(String source, String input) throws GenericParseException {
-		ByteArrayInputStream fis = new ByteArrayInputStream(source.getBytes());
-		lf2.plp.functional2.Programa prog;
-		if (func2Parser == null) {
-			func2Parser = new Func2Parser(fis);
-		} else {
-			Func2Parser.ReInit(fis);
-		}
-		try {
-			prog = Func2Parser.Input();
-		} catch (lf2.plp.functional2.parser.ParseException e) {
-			throw new GenericParseException();
-		}
-
-		return (prog.checaTipo() ? prog.executar().toString()
-				: "Erro det tipos!");
-
+	public String run(String source, String input) {
+		// TODO: Implementar quando tiver Parser disponivel
+		return "Parser nao encontrado";
 	}
 
 }
--- server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc3.java
+++ server/src/main/java/br/ufpe/cin/plp/web/languages/wrappers/WrapperFunc3.java
@@ -1,33 +1,13 @@
 package br.ufpe.cin.plp.web.languages.wrappers;
 
-import java.io.ByteArrayInputStream;
-
-import lf2.plp.functional2.parser.Func2Parser;
-import lf3.plp.functional3.parser.Func3Parser;
-import br.ufpe.cin.plp.web.languages.GenericParseException;
 import br.ufpe.cin.plp.web.languages.Interpreter;
 
 public class WrapperFunc3 implements Interpreter {
-	private Func3Parser func3Parser;
 
 	@Override
-	public String run(String source, String input) throws GenericParseException {
-		ByteArrayInputStream fis = new ByteArrayInputStream(source.getBytes());
-		lf3.plp.functional3.Programa prog;
-		if (func3Parser == null) {
-			func3Parser = new Func3Parser(fis);
-		} else {
-			Func3Parser.ReInit(fis);
-		}
-		try {
-			prog = Func3Parser.Input();
-		} catch (lf3.plp.functional3.parser.ParseException e) {
-			throw new GenericParseException();
-		}
-
-		return (prog.checaTipo() ? prog.executar().toString()
-				: "Erro det tipos!");
-
+	public String run(String source, String input) {
+		// TODO: Implementar quando tiver Parser disponivel
+		return "Parser nao encontrado";
 	}
 
 }
