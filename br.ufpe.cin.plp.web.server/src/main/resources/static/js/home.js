66fce502f1cc0498248aefa0be54a112e9cf07b8
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-18 14:58:45 -0300

    Refactoring e deletado arquivos desnecessarios

--- br.ufpe.cin.plp.web.server/src/main/resources/static/js/home.js
+++ br.ufpe.cin.plp.web.server/src/main/resources/static/js/home.js
@@ -1,34 +1,6 @@
-function execute() {
-	var lin = $("#lang-selector option:selected").val();
-	var aux = editor.getValue();
-	var padrao = $("#stdin-input").val();
-	var output = $("#stdout-list")
-	$.ajax({
-		type : "GET",
-		url : "/compile",
-		dataType : 'JSON',
-		data : {
-			language : lin,
-			input : padrao,
-			source : aux
-		}
-	}).done(function(data) {
-		// Update console
-		output.append("<li>" + data.output + "</li>")
-	})
-}
+(function() {
+	"use strict";
 
-function limpar() {
-	$("#stdout-list").children().remove();
-	$("#stdin-input").val("");
-	editor.setValue("");
-}
-
-function showStdin() {
-	$("#stdin").toggle("slow");
-}
-
-function getLinguagem(sel) {
 	var linguagens = {
 		'exp1': "text/exp1",
 		'exp2': "text/exp2",
@@ -40,15 +12,51 @@ function getLinguagem(sel) {
 		'oo1': "text/oo1",
 		'oo2': "text/oo2"
 	};
-	var linguagem = linguagens[$('#lang-selector').val()];
 
-	if (linguagem) {
-		setLanguage(linguagem);
-	}
-}
+	var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+		lineNumbers: true,
+		matchBrackets: true,
+		mode: "text/exp1",
+		extraKeys: {
+			"Ctrl-Space": "autocomplete"
+		}
+	});
 
-function setLanguage(language){
-	var editor = $('.CodeMirror')[0].CodeMirror;
-	editor.setOption("mode", language);
-	editor.setOption({"Ctrl-Space": "autocomplete"});
-}
+	$("#executar-btn").click(function() {
+		var lin = $("#lang-selector option:selected").val();
+		var aux = editor.getValue();
+		var padrao = $("#stdin-input").val();
+		$.ajax({
+			type : "GET",
+			url : "/compile",
+			dataType : 'JSON',
+			data : {
+				language : lin,
+				input : padrao,
+				source : aux
+			}
+		}).done(function(data) {
+			$("#stdout-list").append("<li>" + data.output + "</li>")
+		});
+	});
+
+	$("#mostrar-btn").click(function() {
+		$("#stdin").toggle("slow");
+	});
+
+	$("#limpar-btn").click(function() {
+		$("#stdout-list").children().remove();
+		$("#stdin-input").val("");
+		editor.setValue("");
+	});
+
+	$("#lang-selector").change(function() {
+		var lingSelectionada = linguagens[$('#lang-selector').val()];
+		if (lingSelectionada) {
+			var editor = $('.CodeMirror')[0].CodeMirror;
+			editor.setOption("mode", lingSelectionada);
+			editor.setOption({"Ctrl-Space": "autocomplete"});
+		}
+	});
+
+})();
